304c619d6846cf6e71998714a97eb887a7f2ab47
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-11-22 23:58:50 +0200

    GraphsController - initial

---
+++ GraphDrawer/Controllers/GraphsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphDrawer.Domain.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphDrawer.Controllers
+{
+    [Route("api/[controller]")]
+    public class GraphsController : Controller
+    {
+        // GET api/graphs/default
+        [HttpGet]
+        [Route("default")]
+        public GraphNode GetDefault()
+        {
+            return new GraphNode
+            {
+                Name = "First",
+                ChildNodes = new[]
+                {
+                    new GraphNode { Name = "F1" },
+                    new GraphNode { Name = "F2" },
+                }
+            };
+        }
+    }
+}
---
+++ GraphDrawer/Domain/Common/Models/GraphNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDrawer.Domain.Common.Models
+{
+    public class GraphNode
+    {
+        public string Name { get; set; }
+        public GraphNode[] ChildNodes { get; set; }
+    }
+}
